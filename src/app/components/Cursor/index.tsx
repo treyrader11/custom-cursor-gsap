6ba19e8b7d5db6c6e4e33b182861a7cdaec9910d
‘Trey <treyrdr09@gmail.com>
2024-06-06 16:28:10 -0500

    clean up

--- src/app/components/Cursor/index.tsx
+++ src/app/components/Cursor/index.tsx
@@ -1,73 +1,3 @@
-// "use client";
-
-// import { cn } from "@/lib/utils";
-// import gsap from "gsap";
-// import { useEffect, useRef } from "react";
-// import ScrollTrigger from "gsap/dist/ScrollTrigger";
-
-// export default function Cursor() {
-//   const cursor = useRef<HTMLDivElement>(null);
-//   const follower = useRef<HTMLDivElement>(null);
-
-//   const moveCursor = (e: MouseEvent): void => {
-//     gsap.to(cursor.current, {
-//       x: e.clientX,
-//       y: e.clientY,
-//       duration: 0.2,
-//     });
-
-//     gsap.to(follower.current, {
-//       x: e.clientX,
-//       y: e.clientY,
-//       duration: 0.9,
-//     });
-//   };
-
-//   useEffect(() => {
-//     gsap.registerPlugin(ScrollTrigger);
-
-//     gsap.set(cursor, {
-//       xPercent: 100,
-//       yPercent: 100,
-//     });
-
-//     gsap.set(follower, {
-//       xPercent: -20,
-//       yPercent: -20,
-//     });
-//     window.addEventListener("mouseover", moveCursor);
-//   }, []);
-
-//   return (
-//     <div className={cn("")}>
-//       <div
-//         ref={cursor}
-//         className={cn(
-//           "cursor",
-//           "size-1.5",
-//           "rounded-full",
-//           "fixed",
-//           "z-[999]",
-//           "bg-white"
-//         )}
-//       ></div>
-//       <div
-//         ref={follower}
-//         className={cn(
-//           "follower-cursor",
-//           "size-7",
-//           "bg-transparent",
-//           "border",
-//           "border-white",
-//           "fixed",
-//           "z-[999]",
-//           "rounded-full"
-//         )}
-//       ></div>
-//     </div>
-//   );
-// }
-
 "use client";
 
 import { cn } from "@/lib/utils";
@@ -112,7 +42,7 @@ export default function Cursor() {
 
     window.addEventListener("mousemove", moveCursor);
 
-    // Cleanup the event listener on component unmount
+    // need to add the cleanup function in useEffect hook to avoid multiples eventlisteners
     return () => {
       window.removeEventListener("mousemove", moveCursor);
     };
@@ -130,7 +60,7 @@ export default function Cursor() {
           "z-[999]",
           "bg-white"
         )}
-      ></div>
+      />
       <div
         ref={follower}
         className={cn(
@@ -143,8 +73,7 @@ export default function Cursor() {
           "z-[999]",
           "rounded-full"
         )}
-      ></div>
+      />
     </div>
   );
 }
-
